56637aa6f9af85c9b8da8b0418601c781206ca4d
Fudge Jordan <fudge@jordan.net.au>
2018-12-19 01:52:44 +1100

    update

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -56,11 +56,6 @@
                 </div>
             </div>
             {!! Form::hidden('type', null, ['id' => 'type']) !!}
-            @if ($agent->isMobile())
-                Mobile
-            @else
-                Bigger
-            @endif
             <table class="table table-hover table-sm table-checkable table-responsive m-table--head-bg-brand m-table" id="datatable1">
                 <thead>
                 <tr>
@@ -108,9 +103,8 @@
         select: true,
         dom: "<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>\n\t\t\t<'row'<'col-sm-12'tr>>\n\t\t\t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
         //buttons: ["print", "copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
-        //dom: 'Bfrtip',
-        @if (!$agent->isMobile())
         buttons: [
+                @if (!$agent->isMobile())
             {
                 text: "<i class='fa fa-cloud-download-alt' style='padding-right: 5px'></i> Export",
                 action: function (e, dt, node, config) {
@@ -123,8 +117,8 @@
                     alert('Import');
                 }
             }
+            @endif
         ],
-        @endif
         ajax: {
             'url': '/data/people',
             'type': 'GET',
