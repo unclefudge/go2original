4d86258ecd68ad33645332b96d25dbdd76f6169d
Fudge Jordan <fudge@jordan.net.au>
2018-12-19 10:19:52 +1100

    update

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -118,7 +118,7 @@
         dom: "<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>\n\t\t\t<'row'<'col-sm-12'tr>>\n\t\t\t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
         //buttons: ["print", "copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
         buttons: [
-                @if (!$agent->isPhone())
+                @if ($agent->isDesktop())
             {
                 text: "<i class='fa fa-cloud-download-alt' style='padding-right: 5px'></i> Export",
                 action: function (e, dt, node, config) {
