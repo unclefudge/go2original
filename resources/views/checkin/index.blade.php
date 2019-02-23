1a88199a687985f66fd5b1c94cb98b1557a893ff
Fudge Jordan <fudge@jordan.net.au>
2019-02-23 21:31:30 +1100

    update

--- resources/views/checkin/index.blade.php
+++ resources/views/checkin/index.blade.php
@@ -84,7 +84,7 @@
 
         @media screen and (max-width: 480px) {
             .people-grid {
-                padding: 2px;
+                padding: 5px;
             }
 
             .people-container {
