f981062b3441959d91044509bf23b6f3066f0fce
Fudge Jordan <fudge@jordan.net.au>
2019-03-25 10:51:03 +1100

    update

--- resources/views/event/view/overview.blade.php
+++ resources/views/event/view/overview.blade.php
@@ -95,11 +95,11 @@
                                                     </div>
                                                 </li>--}}
                                             </ul>
-                                            <div>
-                                                <div class="tab-content">
+
+                                                <div class="tab-content" style="height: 200px; overflow-y: scroll;">
                                                     {{-- New Students --}}
                                                     <div class="tab-pane active show" id="m_tabs_3_1" role="tabpanel">
-                                                        <div class="table-responsive" style="height: 200px; overflow-y: scroll;">
+                                                        <div class="table-responsive">
                                                             <table class="table table-hover">
                                                                 <thead>
                                                                 <tr>
@@ -188,7 +188,6 @@
                                                         something
                                                     </div>
                                                 </div>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
